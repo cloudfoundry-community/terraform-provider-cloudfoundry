96df83c0668a56c96d0116ec54bce7fb1e1e24a7
Mevan Samaratunga <mevansam@gmail.com>
2017-08-12 23:38:25 -0400

    script file to create releases

---
+++ scripts/create-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TAG=$1
+echo $TAG | grep "^[0-9]*\.[0-9]*\.[0-9]*$" 2>&1 > /dev/null
+if [[ $? == 1 ]]; then
+    echo -e "\nUSAGE: ./createRelease.sh [TAG] [BRANCH]"
+    echo -e "\n    TAG     Should be of the format #.#.#"
+    echo -e "    BRANCH  Commits on this branch will be merged to master and tagged for the release\n"
+    exit 1
+fi
+
+set -e
+
+MERGE_BRANCH=$(git branch | awk '/^\*/{ print $2 }')
+if [[ $MERGE_BRANCH == "master" ]]; then
+    echo "Cannot create a release from the master branch. Release is created by merging changes from a feature branch."
+    exit 1
+fi
+trap 'git checkout $MERGE_BRANCH' INT TERM EXIT
+
+echo "Creating release $TAG by merging changes from branch $MERGE_BRANCH"
+
+git checkout master
+git pull
+git merge $MERGE_BRANCH
+git tag -a $TAG -m "Cloud Foundry Terraform provider release $TAG - created by $(git config user.name)"
+git push --follow-tags
+
+git checkout $MERGE_BRANCH
+
+set +e
