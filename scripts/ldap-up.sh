b3f51d8c66d3e1920b4f27cd4e3e12d68c866923
Mevan Samaratunga <mevansam@gmail.com>
2017-08-27 13:26:06 +0400

    unzip ldap database with seed users before starting openldap in docker

--- scripts/ldap-up.sh
+++ scripts/ldap-up.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+set -e
+
 HOST_IP=$(ifconfig | awk '/inet.*[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/&& $2 != "127.0.0.1" { print $2 }' | head -1)
+
+rm -fr .test_env/ldap
+unzip .test_env/ldap.zip -d .test_env
 
 docker run --name my-openldap-container --detach \
     -p 40389:389 \
@@ -12,3 +17,5 @@ docker run --name my-phpldapadmin-container --detach \
     --env PHPLDAPADMIN_LDAP_HOSTS="#PYTHON2BASH:[{'$HOST_IP': [{'server': [{'port': 40389}]},{'login': [{'bind_id': 'cn=admin,dc=example,dc=org'}]}]}]" \
     -p 6443:443 \
     osixia/phpldapadmin:0.7.0
+
+set +e
