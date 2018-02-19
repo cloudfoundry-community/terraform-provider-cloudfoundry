7a997613e5716b1a9efce6bc03caa6bd2d98bcda
ArthurHlt <arthur.halet@orange.com>
2018-02-19 16:19:08 +0100

    fix acceptance test for import space

--- cloudfoundry/import_cf_space_test.go
+++ cloudfoundry/import_cf_space_test.go
@@ -3,21 +3,11 @@ package cloudfoundry
 import (
 	"testing"
 
-	"fmt"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccSpace_importBasic(t *testing.T) {
 	resourceName := "cf_space.space1"
-
-	toIgnore := []string{"staging_asgs.#"}
-
-	session := testSession()
-	asm := session.ASGManager()
-	secGroup, err := asm.Read("public_networks")
-	if err == nil && secGroup.GUID != "" {
-		toIgnore = append(toIgnore, fmt.Sprintf("staging_asgs.%d", resourceStringHash(secGroup.GUID)))
-	}
 
 	resource.Test(t,
 		resource.TestCase{
@@ -25,16 +15,14 @@ func TestAccSpace_importBasic(t *testing.T) {
 			Providers:    testAccProviders,
 			CheckDestroy: testAccCheckSpaceDestroyed("space-one"),
 			Steps: []resource.TestStep{
-
 				resource.TestStep{
 					Config: spaceResource,
 				},
-
 				resource.TestStep{
 					ResourceName:            resourceName,
 					ImportState:             true,
 					ImportStateVerify:       true,
-					ImportStateVerifyIgnore: toIgnore,
+					ImportStateVerifyIgnore: []string{"staging_asgs"},
 				},
 			},
 		})
