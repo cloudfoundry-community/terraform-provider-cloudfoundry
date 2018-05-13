8a24e5aee3b8513b0dd69954db9fa1cadc27685e
Mevan Samaratunga <mevansam@gmail.com>
2018-05-13 22:18:18 +0100

    run acceptance test when a pull request is submitted

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -z "$TRAVIS_TAG" ]] ; then
-    echo "Git commit does not have a release tag so acceptance tests will not run."
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && -z "$TRAVIS_TAG" ]] ; then
+    echo "Git commit is not a pull request or it does not have a release tag so acceptance tests will not run."
     
     make build
     exit $?
