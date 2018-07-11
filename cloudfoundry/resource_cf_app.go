873325297b5b21c20485e716ee5ed0c721810b8d
Jim Carrothers <jim.carrothers@sap.com>
2018-07-11 11:40:20 -0700

    don't error in the case that a service binding has already been removed

--- cloudfoundry/resource_cf_app.go
+++ cloudfoundry/resource_cf_app.go
@@ -980,7 +980,9 @@ func removeServiceBindings(delete []map[string]interface{},
 		if len(bindingID) > 0 {
 			log.DebugMessage("Deleting binding with id '%s' for service instance '%s'.", bindingID, serviceInstanceID)
 			if err := am.DeleteServiceBinding(bindingID); err != nil {
-				return err
+				if !strings.Contains(err.Error(), "status code: 404") {
+					return err
+				}
 			}
 		} else {
 			log.DebugMessage("Ignoring binding for service instance '%s' as no corresponding binding id was found.", serviceInstanceID)
