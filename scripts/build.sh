af88c97f0a1da944c431fec2926eee7e731c5402
Janos Binder <janos.binder@sap.com>
2018-07-17 16:56:52 +0200

    Extended build.sh to run automatically on commits to dev or master

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && -z "$TRAVIS_TAG" ]] ; then
-    echo "Git commit is not a pull request or it does not have a release tag so acceptance tests will not run."
+GIT_BRANCH=`git symbolic-ref -q --short HEAD 2> /dev/null`
+
+if [[ "$GIT_BRANCH" != "dev" && "$GIT_BRANCH" != "master" && "$TRAVIS_PULL_REQUEST" == "false" && -z "$TRAVIS_TAG" ]] ; then
+    echo "Git commit is not on dev or master branch or it is not a pull request or it does not have a release tag so acceptance tests will not run."
     make build
     exit 0
 fi
