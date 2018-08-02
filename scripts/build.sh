025dcbfe420cb0d9e31380ebb57d328bb3812eaa
Janos Binder <janos.binder@sap.com>
2018-08-02 16:36:36 +0200

    See errors2

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-GIT_BRANCH=`git symbolic-ref -q --short HEAD 2> /dev/null`
+# GIT_BRANCH=`git symbolic-ref -q --short HEAD 2> /dev/null`
+GIT_BRANCH="blablabla"
 
 if [[ "$GIT_BRANCH" != "dev" && "$GIT_BRANCH" != "master" && "$TRAVIS_PULL_REQUEST" == "false" && -z "$TRAVIS_TAG" ]] ; then
     echo "Git commit is not on dev or master branch or it is not a pull request or it does not have a release tag so acceptance tests will not run."
