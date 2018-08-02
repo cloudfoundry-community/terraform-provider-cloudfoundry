95bbc5e4afcd45c6577eeb434f4b9751914ca787
Janos Binder <janos.binder@sap.com>
2018-08-02 17:38:59 +0200

    Fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 set -e
-set -x
 
-echo $TRAVIS_BRANCH
+function makeRelease {
+    tests/clean.sh
+    make testacc
+    make release
+}
 
-if [[ "$TRAVIS_BRANCH" != "dev" && "$TRAVIS_BRANCH" != "master" && "$TRAVIS_PULL_REQUEST" == "false" && -z "$TRAVIS_TAG" ]] ; then
-    echo "Git commit is not on dev or master branch or it is not a pull request or it does not have a release tag so acceptance tests will not run."
-    make build
+if [[ "$TRAVIS_BRANCH" == "dev" || "$TRAVIS_BRANCH" == "master" && ]] ; then
+    echo "Git commit is on dev or master branch - building and running the acceptance tests"
+    makeRelease
     exit 0
 fi
 
-tests/clean.sh
-make testacc
-make release
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]] ; then
+    echo "Git commit is a pull request - building and running the acceptance tests"
+    makeRelease
+    exit 0
+fi
+
+if [[ -n "$TRAVIS_TAG" ]] ; then
+    echo "Git commit has a release tag - building and running the acceptance tests"
+    makeRelease
+    exit 0
+fi
+
+echo "Git commit is not on dev or master branch or it is not a pull request or it does not have a release tag so acceptance tests will not run."
+make build
 
