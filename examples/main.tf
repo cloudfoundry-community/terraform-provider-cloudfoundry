8ad9d31a0b6cb4ba040ab989040dfa9457b877fd
Bhanu Pratap Sharan <bhanu.pratap.sharan@sap.com>
2022-08-18 17:48:34 +0530

    Test for space

--- examples/main.tf
+++ examples/main.tf
@@ -1,24 +1,42 @@
 variable "sso_passcode" {}
 
 provider "cloudfoundry" {
-    api_url              = "https://api.cf.sap.hana.ondemand.com"
-    sso_passcode         = var.sso_passcode
-    store_tokens_path    = "tokens.txt"
+  api_url           = "https://api.cf.sap.hana.ondemand.com"
+  sso_passcode      = var.sso_passcode
+  store_tokens_path = "tokens.txt"
 }
 
-data "cloudfoundry_app_v3" "app_test" {
-    name_or_id = "approuter"
-    space = "21352b88-590f-44f9-99c1-ad5967644764"
-}
+# data "cloudfoundry_app_v3" "app_test" {
+#   name_or_id = "approuter"
+#   space      = "21352b88-590f-44f9-99c1-ad5967644764"
+# }
 
 data "cloudfoundry_org_v3" "org_test" {
-    name = "ipa-od--chryso"
+  name = "ipa-deploy--od"
 }
 
-output "app_test_output" {
-    value = data.cloudfoundry_app_v3.app_test.id
+data "cloudfoundry_space" "s" {
+  name = "master"
+  org  = data.cloudfoundry_org_v3.org_test.id
 }
+
+data "cloudfoundry_space_v3" "space_test" {
+  name = "master"
+  org  = data.cloudfoundry_org_v3.org_test.id
+}
+
+# output "app_test_output" {
+#   value = data.cloudfoundry_app_v3.app_test.id
+# }
 
 output "org_test_output" {
-    value = data.cloudfoundry_org_v3.org_test
+  value = data.cloudfoundry_org_v3.org_test
+}
+
+output "space_test_output" {
+  value = data.cloudfoundry_space.s
+}
+
+output "space_test_output" {
+  value = data.cloudfoundry_space_v3.space_test
 }
